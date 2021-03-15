085eb78626e5e0522c82f7a59a00dfba44e3f9e4
Husafasamus <lukassokol@outlook.sk>
2021-03-15 14:32:10 +0100

    v2

--- Creator/ObjectDetector.py
+++ Creator/ObjectDetector.py
@@ -1,8 +1,74 @@
+import torch
+
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+from typing import Iterable
+
+class BBox:
+
+    def __init__(self, x: float, y: float, w: float, h: float) -> None:
+        """
+
+            :param x:
+            :param y:
+            :param w:
+            :param h:
+            """
+        # Kontrola zapornych cisiel?
+        self.x, self.y, self.w, self.h = x, y, w, h
 
 
+class Detection:
+
+    def __init__(self, bbox: BBox, confidence: float, label: str):
+        self.bbox = bbox
+        self.confidence = confidence
+        self.label = label
 
 
 class ObjectDetector:
+    """
+    This class do somethign awesome, but not yet.
+    """
+    pass
+
+
+class SSD300:
+    """
+    Labels -> [bicycle, car, motorcycle, bus, truck]
+    """
 
     def __init__(self):
-        pass
+        self.precision = 'fp32'
+        self.ssd_model = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd', model_math=self.precision)
+        self.utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd_processing_utils')
+        self.ssd_model.to('cuda')
+        self.ssd_model.eval()
+        self.classes_to_labels = self.utils.get_coco_object_dictionary()
+
+    def start(self, uris: Iterable, const_confidence: float) -> 'Detection':
+        """
+        return type class Detection[] -> bbboxes + confidences + labels
+        """
+        detections = []
+        inputs = [self.utils.prepare_input(uri) for uri in uris]
+        tensor = self.utils.prepare_tensor(inputs, self.precision == 'fp16')
+        with torch.no_grad():
+            detections_batch = self.ssd_model(tensor)
+
+        results_per_input = self.utils.decode_results(detections_batch)
+        best_results_per_input = [self.utils.pick_best(results, const_confidence) for results in results_per_input]
+        # best_results_per_input = [self.utils.pick_best(results, 0.40) for results in results_per_input]
+        for image_idx in range(len(best_results_per_input)):
+            bboxes, classes, confidences = best_results_per_input[image_idx]
+
+            bboxes_on_picture = []
+
+            for idx in range(len(bboxes)):
+                left, bot, right, top = bboxes[idx]
+                x, y, w, h = [val * 300 for val in [left, bot, right - left, top - bot]]
+                bboxes_on_picture.append \
+                    (Detection(BBox(x, y, w, h), confidences[idx] * 100, self.classes_to_labels[classes[idx] - 1]))
+            detections.append(bboxes_on_picture)
+
+        return detections
--- Creator/main.py
+++ Creator/main.py
@@ -1,52 +1,105 @@
-from Creator import Handler as hn
 from Creator import Dataset as ds
-from Creator.Reader import FileManager as fm
-import os
+from Creator import ObjectDetector as od
+
+import pathlib
 import json
+import shutil
 
 
+unit = ds.Unit(r'D:\bakalarkaaaa\Merget_datasets')
+ds1 = ds.FakeDataset1(r'D:\bakalarkaaaa\Datasets\Fake_dataset_1')
+ds1.find_images()
 
-if os.path.isdir("D:/bakalarkaaaa/MOJDATASET/"):
-    pass
-if os.path.isdir("D:/bakalarkaaaa/MOJDATASET2/"):
-    pass
+ssd = od.SSD300()
 
-
-
-
-fejkDs1 = ds.FejkDataset("FejkDataset1", "D:/bakalarkaaaa/MOJDATASET/")
-fejkDs2 = ds.FejkDataset("FejkDataset2", "D:/bakalarkaaaa/MOJDATASET2/")
-
-
-
-#fejkDs.find_images()
-#print(os.getcwd())
-#rd.FileManager.create_dir("../NEWDATASET")
-
-
-
-
-#index = 1
-#for item in fejkDs.images:
-#    rd.FileManager.copy_move(fr"{item}", fr"D:\bakalarkaaaa\NEWDATASET\{index}.jpg")
-#    index+=1
-
-#rd.FileManager.rename(r"D:\bakalarkaaaa\NEWDATASET\1.jpg", r"D:\bakalarkaaaa\NEWDATASET\x.jpg")
-
-handler = hn.Handler("../NEWDATASET", fejkDs1, fejkDs2, "x")
-handler.merge()
-
-#fm.create_file("../new.json")
-
-
-
-#json.loads(data)
+pathlib.Path(r'D:\bakalarkaaaa\Datasets\Detections').mkdir(exist_ok=True)
+pathlib.Path(r'D:\bakalarkaaaa\Datasets\Detections\Fake_dataset_1').mkdir(exist_ok=True)
+p = pathlib.Path(r'D:\bakalarkaaaa\Datasets\Detections\Fake_dataset_1\ssd')
+p.mkdir(exist_ok=True)
+p.joinpath('detections.json').touch(exist_ok=True)
+p = pathlib.Path(r'D:\bakalarkaaaa\Datasets\Detections\Fake_dataset_1\ssd\detections.json')
+unit.create_json_from_detections(ssd.start(ds1.paths_to_images, 0.4), ds1.paths_to_images,p)
 
 
 
 
+r"""
+path_to_datasets = pathlib.Path(r'D:\bakalarkaaaa\Datasets')
+
+pathlib.Path()
 
 
+if not path_to_datasets.exists():
+    exit("Path to datasets does not exists!")
+
+# Vytvorenie priecinku pre mergnuty dataset
+pathlib.Path(r'D:\bakalarkaaaa\Merget_datasets').mkdir(exist_ok=True)
+
+#
+unit = ds.Unit(r'D:\bakalarkaaaa\Merget_datasets')
+
+# Inicializacia datasetov
+ds1 = ds.FakeDataset1(r'D:\bakalarkaaaa\Datasets\Fake_dataset_1')
+ds2 = ds.FakeDataset1(r'D:\bakalarkaaaa\Datasets\Fake_dataset_2')
+
+# Najde si cesty ku fotkam
+ds1.find_images()
+ds2.find_images()
+
+# Provizorne vycuca lable uz vytvoreneho datasetu
+unit.add_labels_from_ds(ds1.get_labels()['content'])
+unit.add_labels_from_ds(ds2.get_labels()['content'])
+
+# Prekopiruje obrazky z datasetu
+unit.copy_imgs_from_ds(ds1)
+unit.copy_imgs_from_ds(ds2)
+
+"""
+
+"""
+
+import torch
+precision = 'fp32'
+ssd_model = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd', model_math=precision)
+
+utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd_processing_utils')
+
+ssd_model.to('cuda')
+ssd_model.eval()
 
 
+uris = ds1.paths_to_images
 
+inputs = [utils.prepare_input(uri) for uri in uris]
+tensor = utils.prepare_tensor(inputs, precision == 'fp16')
+
+with torch.no_grad():
+    detections_batch = ssd_model(tensor)
+
+results_per_input = utils.decode_results(detections_batch)
+best_results_per_input = [utils.pick_best(results, 0.1) for results in results_per_input]
+
+classes_to_labels = utils.get_coco_object_dictionary()
+
+
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+
+for image_idx in range(len(best_results_per_input)):
+    fig, ax = plt.subplots(1)
+    # Show original, denormalized image...
+    image = inputs[image_idx] / 2 + 0.5
+    ax.imshow(image)
+    # ...with detections
+    bboxes, classes, confidences = best_results_per_input[image_idx]
+    print(bboxes)
+    print(classes)
+    print(confidences)
+    print(image_idx)
+    for idx in range(len(bboxes)):
+        left, bot, right, top = bboxes[idx]
+        x, y, w, h = [val * 300 for val in [left, bot, right - left, top - bot]]
+        rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        ax.text(x, y, "{} {:.0f}%".format(classes_to_labels[classes[idx] - 1], confidences[idx]*100), bbox=dict(facecolor='white', alpha=0.5))
+plt.show()"""
